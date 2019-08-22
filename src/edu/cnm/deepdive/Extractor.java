9bda8e0704836c8d12bcdb8c79d12ba1455a9ff9
Brian <babta29@gmail.com>
2019-08-22 10:12:18 -0600

    expression lambda in treeset constructor invocation

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -7,14 +7,8 @@ import java.util.TreeSet;
 public class Extractor {
 
   public static void main(String[] args) {
-
     String source = (args.length > 0) ? args[0] : "Hello, World!";
-    Set<Character> chars = new TreeSet<>(new Comparator<Character>() {
-      @Override
-      public int compare(Character c1, Character c2) {
-        return Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2));
-      }
-    });
+    Set<Character> chars = new TreeSet<>((c1, c2) -> Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2)));
     for (char c : source.toCharArray()) {
       chars.add(c);
     }
